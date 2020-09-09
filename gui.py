f7370eca5309232362989d44a037de64f5c6170b
Abhay Patel <patel.abhay1@hotmail.com>
2020-09-09 15:46:12 -0400

    Changed window size gui for different resoultions

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ class GUIWindow:
         self.root = root
         root.title("A simple GUI")
 
-        self.label = Label(root, text="This is our first GUI!")
+        self.label = Label(root, text="Sudoku Solver")
 
         self.greet_button = Button(root, text="Greet", command=self.greet)
 
@@ -20,6 +20,23 @@ class GUIWindow:
         print("Hello")
 
 
+def setWindow(root):
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    window_width = screen_width//2
+    window_height = screen_height//2
+
+    x_offset = (screen_width//2) - (screen_width//4)
+    y_offset = (screen_height//2) - (screen_height//4)
+
+    window_size = str(window_width) + "x" + str(window_height)
+    window_offset = str(x_offset) + "+" + str(y_offset)
+
+    root.geometry(window_size + "+" + window_offset)
+
+
 root = Tk()
+setWindow(root)
 gui = GUIWindow(root)
 root.mainloop()
